829daafbdcb414eafea79122cd7d172c1e0cda90
Nathan Pickard <nathan.pickard87@gmail.com>
2018-07-06 22:36:12 -0700

    Adding venues list component

---
+++ src/app/venues/venues-list/venues-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-venues-list',
+  templateUrl: './venues-list.component.html',
+  styleUrls: ['./venues-list.component.css']
+})
+export class VenuesListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/venues/venues.module.ts
+++ src/app/venues/venues.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { VenuesComponent } from './venues.component';
+import { VenuesListComponent } from './venues-list/venues-list.component';
 
 @NgModule({
   declarations: [
-    VenuesComponent
+    VenuesComponent,
+    VenuesListComponent
   ],
   imports: [
     CommonModule,
