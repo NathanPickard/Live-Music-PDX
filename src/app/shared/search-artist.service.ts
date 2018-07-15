bbe4a2725fc9d100900ad236f78b6166cdefd103
Nathan Pickard <nathan.pickard87@gmail.com>
2018-07-14 22:46:28 -0700

    Search artist service and angular flex-layout imported

---
+++ src/app/shared/search-artist.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams, HttpRequest } from '@angular/common/http';
+import 'rxjs/Rx';
+
+import { ArtistService } from '../artists/artist.service';
+import { Artist } from '../artists/artist.model';
+
+@Injectable()
+export class SearchArtistService {
+  constructor(private httpClient: HttpClient, private artistService: ArtistService) { }
+
+
+  getArtists() {
+    this.httpClient.get<Artist[]>('http', {
+      observe: 'body',
+      responseType: 'json'
+    })
+      .subscribe(
+        (artists: Artist[]) => {
+          this.artistService.setArtists(artists);
+        }
+      )
+  }
+}
