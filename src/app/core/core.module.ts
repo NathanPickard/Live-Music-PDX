37489eb6a7baa3edd51ca19af5f95aac3703949f
Nathan Pickard <nathan.pickard87@gmail.com>
2018-07-16 21:44:04 -0700

    routerLinkActive and fxLayoutGap for header

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,6 +3,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button'
 import { MatListModule } from '@angular/material/list';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
@@ -20,7 +21,8 @@ import { VenueService } from '../venues/venue.service';
     MatIconModule,
     MatButtonModule,
     MatListModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule
   ],
   exports: [
     AppRoutingModule,
