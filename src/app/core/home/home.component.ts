3d5175d012e66940032e896ce005d6a54779199b
Nathan Pickard <nathan.pickard87@gmail.com>
2018-08-02 22:21:08 -0700

    Implementing upcoming PDX events for homepage

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,8 +1,19 @@
 import { Component } from '@angular/core';
 
+import { SearchService } from '../../shared/search.service';
+
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 
-export class HomeComponent { }
+export class HomeComponent {
+
+  constructor(private searchService: SearchService) { }
+
+  getPdxEvents() {
+    return this.searchService.getPdxEvents().subscribe()
+  }
+
+}
--- src/app/shared/search.service.ts
+++ src/app/shared/search.service.ts
@@ -15,7 +15,7 @@ export class SearchService {
   private API_KEY: string = environment.SONGKICK_API_KEY;
   private API_URL: string = environment.SONGKICK_API_URL;
   private ARTIST_URL: string = this.API_URL + 'search/artists.json?apikey=' + this.API_KEY + '&query=';
-  private VENUE_URL: string =this.API_URL + 'search/venues.json?query=';
+  private VENUE_URL: string = this.API_URL + 'search/venues.json?query=';
 
   constructor(private httpClient: HttpClient, private http: Http, private artistService: ArtistService) { }
 
@@ -37,6 +37,11 @@ export class SearchService {
 
   getVenues(query) {
     return this.http.get(this.VENUE_URL + query + '&apikey=' + this.API_KEY)
+      .map(res => res.json());
+  }
+
+  getPdxEvents() {
+    return this.http.get(this.API_URL + 'metro_areas/12283/calendar.json?apikey=' + this.API_KEY)
       .map(res => res.json());
   }
 }
