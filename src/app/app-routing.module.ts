eb8081883b873c327dbcf4b6c2a4e6b2cf221463
Nathan Pickard <nathan.pickard87@gmail.com>
2018-08-20 20:29:33 -0700

    register route adjusted in app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { HomeComponent } from './core/home/home.component';
-import { RegisterComponent } from './auth/register/register.component';
+// import { RegisterComponent } from './auth/register/register.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'artists', loadChildren: './artists/artists.module#ArtistsModule' },
   { path: 'venues', loadChildren: './venues/venues.module#VenuesModule' },
-  { path: 'register', component: RegisterComponent}
+  // { path: 'register', component: RegisterComponent}
 ];
 
 @NgModule({
