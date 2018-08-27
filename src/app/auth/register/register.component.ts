0dde8aaef57aee4095949d02734a88dac5b557fb
Nathan Pickard <nathan.pickard87@gmail.com>
2018-08-26 22:55:56 -0700

    Reactive forms for registration

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { NgForm, FormControl, Validators, FormGroup } from '@angular/forms';
 
 import { AuthService } from '../auth.service';
 
@@ -12,9 +12,27 @@ export class RegisterComponent {
 
   constructor(private authService: AuthService) { }
 
-  onRegister(form: NgForm) {
-    const email = form.value.email;
-    const password = form.value.password;
-    this.authService.registerUser(email, password);
+  // email = new FormControl('', [Validators.required, Validators.email]);
+  registerForm: FormGroup;
+
+  // onRegister(form: NgForm) {
+  //   const email = form.value.email;
+  //   const password = form.value.password;
+  //   this.authService.registerUser(email, password);
+  // }
+
+  ngOnInit() {
+    this.registerForm = new FormGroup({
+      'userData': new FormGroup({
+        'email': new FormControl(null, [Validators.required, Validators.email]),
+        'password': new FormControl(null, [Validators.required, Validators.minLength(6)])
+      })
+    })
+
   }
+
+  // getErrorMessage() {
+  //   return this.registerForm.hasError('required') ? 'You must enter a value' :
+  //     this.registerForm.hasError('email') ? 'Not a valid email' : '';
+  // }
 }
