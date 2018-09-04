bb4a4c8eef310ff089c8c28ec5ce79788fe4d977
Nathan Pickard <nathan.pickard87@gmail.com>
2018-09-03 22:56:50 -0700

    Modifying the request in the auth interceptor via clone()

--- src/app/shared/auth.interceptor.ts
+++ src/app/shared/auth.interceptor.ts
@@ -1,9 +1,18 @@
 import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { Injectable } from "@angular/core";
+
+import { AuthService } from "../auth/auth.service";
+
+@Injectable()
 
 export class AuthInterceptor implements HttpInterceptor {
+  constructor(private authService: AuthService) { }
+
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log('Intercepted', req);
-    return next.handle(req);
+    // const copiedReq = req.clone({ headers: req.headers.set('', '') });
+    const copiedReq = req.clone({ params: req.params.set('auth', this.authService.getToken()) });
+    return next.handle(copiedReq);
   }
 }
