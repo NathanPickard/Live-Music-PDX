7805c7932a6dc3bd626c3f437e005289247f5ce8
Nathan Pickard <nathan.pickard87@gmail.com>
2018-10-17 19:07:42 -0700

    Route transition animation started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeAnimation } from './animations';
 import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [fadeAnimation]
 })
 export class AppComponent implements OnInit {
   loadedFeature = 'artist';
