ff66d137bb7962f3cefcc6f68376889c2e24419d
Nathan Pickard <nathan.pickard87@gmail.com>
2019-01-11 22:11:45 -0800

    About page information and homepage search results positioning adjustments

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -8,6 +8,7 @@ import { MatButtonModule } from '@angular/material/button'
 import { MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
@@ -49,6 +50,7 @@ import { AboutComponent } from './about/about.component';
     MatCardModule,
     MatExpansionModule,
     MatFormFieldModule,
+    MatGridListModule,
     MatIconModule,
     MatInputModule,
     MatListModule,
