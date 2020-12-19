7a0333cf6b48a8b6406646afc1e55af427f72caa
Nathan Pickard <nathan.pickard87@gmail.com>
2020-12-18 19:54:22 -0800

    Import cleanup

--- src/app/shared/search.service.ts
+++ src/app/shared/search.service.ts
@@ -9,9 +9,7 @@ import 'rxjs/add/operator/map';
 import { ArtistService } from '../artists/artist.service';
 import { Artist } from '../artists/artist.model';
 import { Venue } from '../venues/venue.model';
-// import { Observable } from 'rxjs/Rx';
 import { Observable } from 'rxjs';
-// import 'rxjs/add/observable/forkJoin';
 
 @Injectable()
 export class SearchService {
